2783d6bce650d73a625b20a500ffca584d066362
Juan Agustin Alejandro <juan_agustin_alejandro@hotmail.com>
2022-02-09 11:57:51 -0300

    Publicaciones
    Se agrego la modificación de publicación

---
+++ app/publicaciones.js
@@ -0,0 +1,94 @@
+class Receta{
+    constructor(titulo, ingrediente, preparacion, imagen, autor) {
+        this.id = new Date().getTime()
+        this.titulo = titulo
+        this.ingrediente = ingrediente
+        this.preparacion = preparacion
+        this.imagen = imagen
+        this.autor = autor
+        this.like = {
+            usuarioLike : [],
+            cantidad : 0
+        }
+    }
+   
+}
+
+const crear_card = function () {
+    let contenedor_cards = document.querySelector('#contenedor_cards')
+    let cardTrago = JSON.parse(localStorage.getItem('recetas')) || []
+    let card = ''
+    cardTrago.map(function(item){
+    card +=` <div class="accordion " id="accordionExample">
+                
+    <div class="accordion-item mb-3 card-publicacion">
+      <div class="card mb-3 boxHome ">
+        <div class="row g-0 ">
+          <div class="col-md-4">
+            <img src=${item.imagen} class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8 align-self-center ">
+            <div class="card-body d-flex flex-column align-items-center ">
+              <h2 class="card-title "><strong>${item.titulo}</strong></h2>
+              <samp class="blockquote-footer text-center my-2">${item.autor}</samp>
+              <p class="card-text">${item.ingrediente}</p>
+            </div>
+          </div>
+
+        </div>
+        <div class="accordion-header" id="headingOne">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          </button>
+      </div>
+      </div>
+      <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+        <div class="accordion-body">
+          <h4 class="card-text mb-3"><strong>Preparacion</strong></h4>
+          <p class="card-text"> ${item.preparacion}</p>
+        </div>
+      </div>
+    </div>
+
+    
+  </div>`
+    
+});
+contenedor_cards.innerHTML=card;
+}
+
+
+
+let myModal = new bootstrap.Modal(document.getElementById('nuevaPublic'), {
+    keyboard: false
+  });
+document.querySelector('#addPublic').addEventListener('click', function(){
+  myModal.show()
+});
+const agregarImagen = function(e){
+let campo=document.querySelector('#text_modal')
+if(e.keyCode===13){
+document.querySelector('#img_modal').src=campo.value
+}
+};
+document.querySelector('#imgPrueba').addEventListener('keydown', agregarImagen);
+const guardarPublicacion = function () {
+    let user = JSON.parse(localStorage.getItem('user')) || []
+    let recetas = JSON.parse(localStorage.getItem('recetas')) || []
+
+
+    let titulo = document.getElementById('modal_titulo').value
+    let ingredientes = document.getElementById('modal_ingrediente').value.split('-')
+  
+    let preparacion = document.getElementById('modal_preparacion').value
+    let imagen = document.getElementById('imgPrueba').value
+  
+  
+  
+    recetas.unshift(new Receta(titulo, ingredientes, preparacion, imagen, user.nick))
+    localStorage.setItem('recetas', JSON.stringify(recetas))
+crear_card();
+    
+};
+
+
+crear_card();
